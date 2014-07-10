46858388c50dfe3edacbd2215219ed627c15e71d
Jerry Kim <jerrykskim@gmail.com>
2014-07-09 23:19:32 -0700

    add nltk tokenizer

--- src/mole.py
+++ src/mole.py
@@ -1,34 +1,50 @@
 __author__ = 'jkim'
 
+
 from reppy.cache import RobotsCache
 from beautifulscraper import BeautifulScraper
-import urllib2
+import nltk
+
 
 class Mole:
-    ''' fetch web page based on robots.txt'''
+    """ fetch web page based on robots.txt """
+
     agent = "jerry's crawler"
 
     def __init__(self):
         self.robots = RobotsCache()
+        self.scraper = BeautifulScraper()
 
-    def fetch(self,url):
-        if self.robots.allowed(url,Mole.agent):
-            header= { 'User-Agent' : Mole.agent}
-            req=urllib2.Request(url=url,data=None,headers=header)
-            return urllib2.urlopen(req).read()
-
+    def fetch(self, uri):
+        if self.robots.allowed(uri, Mole.agent):
+            self.scraper.add_header('User-Agent', Mole.agent)
+            return self.scraper.go(uri)
         return None
 
+    def access2nasdaq(self):
+        url = 'http://www.nasdaq.com'
+        body = self.fetch(url)
+        ret = []
+        if body is not None:
+            section = body.find('div', {'id' : 'home-editors-pick'})
+
+            for ul in section.findAll('ul'):
+                ret.append(ul.find('a')['href'])
+
+        return ret
+
+
+
 if __name__ == "__main__":
+    import sys
     crawler = Mole()
-    url = 'http://www.nasdaq.com'
-    #html= crawler.fetch(url)
-    #print html
-    scraper = BeautifulScraper()
-    scraper.add_header('User-Agent',Mole.agent)
-    body = scraper.go(url)
-    section = body.find('div',{'id':'home-editors-pick'})
-    for ul in section.findAll('ul'):
-        print ul.find('a')
-
-
+    for article_url in crawler.access2nasdaq():
+        page = crawler.fetch(article_url)
+        if page is not None:
+            raw = nltk.clean_html(str(page))
+            tokens = nltk.word_tokenize(raw)
+            fdist = nltk.FreqDist(tokens)
+            print fdist.N()
+            print fdist.B()
+            print fdist.samples()
+            sys.exit(0)
